f3576bfcd4c75e28cdbd3947fc7144a9f1cd6a62
garyfinco <68601358+garyfinco@users.noreply.github.com>
2022-11-08 15:35:10 -0800

    Add files via upload

--- ndcounties.js
+++ ndcounties.js
@@ -1,22 +1,30 @@
+//Setting width and height of the svg
 var width = 960,
     height = 500;
 
+//Creating SVG
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height + 100);
 
+//Declaring the default color scale
 var color = d3.scaleThreshold()
     .domain([0.0, 0.5, 1.5, 3.0, 7.0, 15.0, 30.0, 80.0])
     .range(d3.schemeOrRd[9]);
 
+//Defining the scale for the legend
 var x = d3.scaleSqrt()
     .domain([0, 100])
     .range([440, 950]);
 
+//Appending a g element to the svg
 var g = svg.append("g")
     .attr("class", "key")
     .attr("transform", "translate(0,550)");
 
+//Creating a rectangle in the g element seting the color segments of the legend
+//Defining the width and height of the rectangle with the scale fucntion x
+//Filling in the colors for each density
 g.selectAll("rect")
   .data(color.range().map(function(d) {
       d = color.invertExtent(d);
@@ -30,6 +38,7 @@ g.selectAll("rect")
     .attr("width", function(d) {console.log("calc d1 " +x(d[1]) + "calc d0 " +x(d[0])); return x(d[1]) - x(d[0]); })
     .attr("fill", function(d) { return color(d[0]); });
 
+//Adding the title to the legend
 g.append("text")
     .attr("class", "caption")
     .attr("x", x.range()[0])
@@ -39,6 +48,7 @@ g.append("text")
     .attr("font-weight", "bold")
     .text("Population Density per County");
 
+//Adding the ticks to the legend to differntiate the densities
 g.call(d3.axisBottom(x)
     .tickSize(13)
     .tickValues(color.domain())
@@ -46,29 +56,36 @@ g.call(d3.axisBottom(x)
   .select(".domain")
     .remove();
 
+//Accessing the data
 d3.json("counties-10m.json", function(error, counties) {
     if (error) return console.error(error);
     
+    //Filtering the counties and putting them in an object so that fitsize can call it
     var nd_counties = topojson.feature(counties, counties.objects.counties).features.filter((d) => parseInt(d.id) >= 38000 && parseInt(d.id) <= 38999);
     var geojson = {"type":"FeatureCollection", "features": nd_counties};
     var projection = d3.geoMercator().scale(1).fitSize([width, height], geojson);
     var path = d3.geoPath().projection(projection);
     console.log(counties);
     
+    //Accessing the density data
     var nd_densities;
     d3.csv("Population-Density By County.csv", function(error, population) {
         if (error) return console.error(error);
         
+        //Filtering the density data for just North Dakota
         var nd_pop = population.filter((d) => d["GEO.display-label"] === "North Dakota");
         console.log(nd_pop);
         nd_densities = nd_pop.map(function(d) { return d["Density per square mile of land area"]});
         
+        //Putting the filter data in objects
         const densities_obj = {};
         const counties_obj = {};
         nd_pop.forEach(d => (densities_obj[d["GCT_STUB.display-label"]] = d["Density per square mile of land area"]));
         nd_counties.forEach(d => (counties_obj[d.properties.name] = d));
         console.log(densities_obj);
     
+        //Drawing the counties that make up the whole state
+        //When mouse over tooltip appears
         svg.selectAll("path")
             .data(nd_counties)
             .enter().append("path")
@@ -91,6 +108,7 @@ d3.json("counties-10m.json", function(error, counties) {
                 d3.select("#tooltip").classed("hidden", true);
             });;
         
+        //When County border is clicked then the borders are highlighted with black stroke
         var checkbox = document.querySelector('input[type="checkbox"]');
 
         checkbox.addEventListener('change', function () {
@@ -101,6 +119,7 @@ d3.json("counties-10m.json", function(error, counties) {
             }
         });
         
+        //When color button is clicked then the map changes color scheme
         var colorbox = document.querySelector('#colorbox');
 
         colorbox.addEventListener('change', function () {
